3a8154a97a0def374d4be4bc63b17165034db1a5
Dave Horsfall <dave@horsfall.dev>
2022-02-08 12:02:54 +0000

    init vitessce config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Vitessce } from "vitessce/dist/umd/production/index.min.js";
+import ViewConfig from "./vitessceConfig.json";
+import "vitessce/dist/umd/production/static/css/index.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Vitessce config={ViewConfig} height={1000} theme="dark" />
   );
 }
 
