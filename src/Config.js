45624787ce30478f24607b284bde16329dec89a5
prete <martin.prete@sanger.ac.uk>
2022-07-04 11:59:40 +0100

    Allow to load config files from URL

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,34 @@
-export function getConfig(config){
-    try{
-        return require(`./config/${config}.json`)
+function isValidURL(config) {
+    // cheapest URL validation I could think of
+    var pattern = new RegExp('^(https?:\\/\\/)(.*?).json$','i'); 
+    return !!pattern.test(config);
+}
+
+async function loadConfigFromURL(configURL) {
+    let response = await fetch(configURL)
+    let configJSON = await response.json();
+    return configJSON;
+}
+
+function loadConfigFromLocal(configName) {
+    // don't try to `../../file.js` me, boy!
+    configName = configName.replace(/^.*[/]/, ''); 
+    return require(`./config/${configName}.json`)
+}
+
+export function getConfig(config) {
+    var configJSON = {};
+    try {
+        if (isValidURL(config)) {
+            configJSON = loadConfigFromURL(config);
+        } else {
+            configJSON = loadConfigFromLocal(config);
+        }
+        if (!config.version) {
+            // add version property if it's missing
+            config.version = "X.Y.Z";
+        }
+        return configJSON;
     }
     catch(error){
         return null
