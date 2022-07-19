cf01f527bb2469e476de83e744085f7c6d265cba
Ubuntu <jc52@sanger.ac.uk>
2022-07-19 09:31:58 +0000

    Start of app testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import App from './App';
+import Viewer from './Viewer';
+
+const { SampleJson } = require('../public/config/iss-human-brain-advanced.json');
 
 test('renders loading text', () => {
   window.URL.createObjectURL = jest.fn();
   render(<App />);
   const linkElement = screen.getByText(/Loading.../i)
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders development text', () => {
+  window.URL.createObjectURL = jest.fn();
+  render(<Viewer />);
+  const linkElement = screen.getByText(/This app is in active development/i)
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders spatial text', () => {
+  window.URL.createObjectURL = jest.fn();
+  const mockConfig = {a:2, b:2, c:3};
+  const viewer = render(<Viewer config={SampleJson} />);
+
+const spatial = viewer.getByText(/Spatial/i);
+expect(spatial).toBeInTheDocument();
 });
