5dedd3761f91ce1c8efad8d4c1626d2b167c02af
Jessica Cox <jc52@sanger.ac.uk>
2022-07-25 11:46:10 +0000

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -30,8 +30,8 @@ beforeEach(() => {
 test("renders loading text", () => {
   window.URL.createObjectURL = jest.fn();
   render(<App />);
-  const loadElement = screen.getByText(/Loading.../i);
-  expect(loadElement).toBeInTheDocument();
+  const element = screen.getByText(/Loading.../i);
+  expect(element).toBeInTheDocument();
 });
 
 //ERROR TESTING
@@ -39,23 +39,23 @@ test("renders loading text", () => {
 test("test viewer without config", () => {
   window.URL.createObjectURL = jest.fn();
   render(<Viewer />);
-  const errorElement = screen.getByText(
+  const element = screen.getByText(
     /The dataset configuration could not be found/);
-  expect(errorElement).toBeInTheDocument();
+  expect(element).toBeInTheDocument();
 });
 
 test("test viewer with wrong version in config", () => {
   window.URL.createObjectURL = jest.fn();
   render(<Viewer config={{ version: "This.Is.An.Invalid.Version" }} />);
-  const errorElement = screen.getByText(/Unknown config version/);
-  expect(errorElement).toBeInTheDocument();
+  const element = screen.getByText(/Unknown config version/);
+  expect(element).toBeInTheDocument();
 });
 
 test("test viwer with wrong initStrategy in config", () => {
   window.URL.createObjectURL = jest.fn();
   render(<Viewer config={incorrectInitStrategy} />);
-  const errorElement = screen.getByText(/should be equal to one of the allowed values/i);
-  expect(errorElement).toBeInTheDocument();
+  const element = screen.getByText(/should be equal to one of the allowed values/i);
+  expect(element).toBeInTheDocument();
 });
 
 
@@ -64,8 +64,8 @@ test("test viwer with wrong initStrategy in config", () => {
 test("test minimal working config", () => {
   window.URL.createObjectURL = jest.fn();
   render(<Viewer config={minimalConfig} />);
-  const linkElement = screen.getByText(/this is a test component/);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/this is a test component/);
+  expect(element).toBeInTheDocument();
 });
 
 
@@ -73,30 +73,30 @@ test("test minimal working config", () => {
 
 test("test Spatial rendered", () => {
   window.URL.createObjectURL = jest.fn();
-  const page = render(<Viewer config={spatialLayout} />);
-  const spatialElement = screen.getByText(/Spatial/i);
-  expect(spatialElement).toBeInTheDocument();
+  render(<Viewer config={spatialLayout} />);
+  const element = screen.getByText(/Spatial/i);
+  expect(element).toBeInTheDocument();
 });
 
 test("test Data Set rendered", () => {
   window.URL.createObjectURL = jest.fn();
   render(<Viewer config={dataSetLayout} />);
-  const dataElement = screen.getByText(/Data Set/i);
-  expect(dataElement).toBeInTheDocument();
+  const element = screen.getByText(/Data Set/i);
+  expect(element).toBeInTheDocument();
 });
 
  test("test Spatial Layers rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={spatialLayersLayout} />);
-  const layersElement = screen.getByText(/Spatial Layers/i);
-  expect(layersElement).toBeInTheDocument();
+  const element = screen.getByText(/Spatial Layers/i);
+  expect(element).toBeInTheDocument();
 });
 
 test("test Status rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={statusLayout} />);
-  const statusElement = screen.getByText(/Status/i);
-  expect(statusElement).toBeInTheDocument();
+  const element = screen.getByText(/Status/i);
+  expect(element).toBeInTheDocument();
 });
 
 /*test("test Heatmap rendered", () => {
@@ -110,42 +110,42 @@ test("test Status rendered", () => {
 test("test Scatterplot rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={scatterplotLayout} />);
-  const scatterElement = screen.getByText(/Scatterplot/i);
-  expect(scatterElement).toBeInTheDocument();
+  const element = screen.getByText(/Scatterplot/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("Expression by Cell Set rendered", () => {
+test("test Expression by Cell Set rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={cellSetExpressionLayout} />);
-  const cellElement = screen.getByText(/Expression by Cell Set/i);
-  expect(cellElement).toBeInTheDocument();
+  const element = screen.getByText(/Expression by Cell Set/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("Expression Levels rendered", () => {
+test("test Expression Levels rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={expressionLevelsLayout} />);
-  const expressionElement = screen.getByText(/Expression Levels/i);
-  expect(expressionElement).toBeInTheDocument();
+  const element = screen.getByText(/Expression Levels/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("Cell Sets rendered", () => {
+test("test Cell Sets rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={cellSetsLayout} />);
-  const cellElement = screen.getByText(/Cell Sets/i);
-  expect(cellElement).toBeInTheDocument();
+  const element = screen.getByText(/Cell Sets/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("Expression Histogram rendered", () => {
+test("test Expression Histogram rendered", () => {
   window.URL.createObjectUrl = jest.fn();
   render(<Viewer config={histogramLayout} />);
-  const histogramElement = screen.getByText(/Expression Histogram/i);
-  expect(histogramElement).toBeInTheDocument();
+  const element = screen.getByText(/Expression Histogram/i);
+  expect(element).toBeInTheDocument();
 });
 
 
 //App.js Testing
 
-test("testing output when incorrect url is injected", async () => {
+test("test passing invalid json file", async () => {
   delete window.location;
   window.location = { search: '?config=http://example.com/doesnt_exist/config.json' };
 
@@ -153,30 +153,24 @@ test("testing output when incorrect url is injected", async () => {
     render(<App />);
   })
   await new Promise((r) => setTimeout(r, 2000));
-  const histogramElement = screen.getByText(/Error fetching/i);
-  expect(histogramElement).toBeInTheDocument();
+  const element = screen.getByText(/Error fetching/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("testing output when problems occur parsing JSON file", async () => {
+test("test passing json file with syntax errors", async () => {
   delete window.location;
   window.location = { search: '?config=http://localhost:3000/config/tests/invalid_json.json' };
-
-  act(() => {
-    render(<App />);
-  })
+  render(<App />);
   await new Promise((r) => setTimeout(r, 2000));
-  const histogramElement = screen.getByText(/Error parsing JSON/i);
-  expect(histogramElement).toBeInTheDocument();
+  const element = screen.getByText(/Error parsing JSON/i);
+  expect(element).toBeInTheDocument();
 });
 
-test("testing that viewer is rendered when json file does not include version", async () => {
+test("test passing json file not including version", async () => {
   delete window.location;
   window.location = { search: '?config=http://localhost:3000/config/tests/no_version.json' };
-
-  act(() => {
-    render(<App />);
-  })
+  render(<App />);
   await new Promise((r) => setTimeout(r, 2000));
-  const histogramElement = screen.getByText(/vitessce-app/i);
-  expect(histogramElement).toBeInTheDocument();
+  const element = screen.getByText(/vitessce-app/i);
+  expect(element).toBeInTheDocument();
 });
