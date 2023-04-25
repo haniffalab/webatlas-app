2210ce8f815b7ce4140fbb28ef527bfa354b6f98
Dave Horsfall <dave@horsfall.dev>
2023-04-25 10:47:06 +0100

    fix #36

--- src/Loader.js
+++ src/Loader.js
@@ -3,9 +3,9 @@ import "./themes/Loader.scss";
 export default function Loader() {
   return (
     <div className="App">
-        <div className="loader">
-            <div class="loader-ring"><div></div><div></div><div></div><div></div></div>
-        </div>
+      <div className="loader">
+        <div className="loader-ring" data-testid="loader-ring"><div></div><div></div><div></div><div></div></div>
+      </div>
     </div>
   );
 }
